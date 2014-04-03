e6d7dbfa1416093cf0e05aa63b07ac5bf857629d
Cameron Bulock <cameron@bulock.com>
2014-04-03 17:49:52 -0400

    Initial commit

---
+++ src/core/ImageFilter/Random.php
@@ -0,0 +1,18 @@
+<?php
+namespace JohnVanOrange\core\ImageFilter;
+
+class Random extends Base {
+
+ public function __construct($options = NULL) {
+  parent::__construct($options);
+ }
+ 
+ protected function sort() {
+  $this->orderBy('RAND()');
+ }
+ 
+ protected function limit_process() {
+  $this->limit(1);
+ }
+
+}
