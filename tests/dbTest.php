04fd48b83972fd9ded1a9d2be5de61e4e48e6ff4
Cameron Bulock <cameron@bulock.com>
2014-04-04 17:01:48 -0400

    getting tests working

---
+++ tests/dbTest.php
@@ -0,0 +1,29 @@
+<?php
+require_once('settings.inc');
+
+class dbTest extends PHPUnit_Framework_TestCase {
+ 
+ protected $db;
+ protected function setUp(){
+  $this->db = new JohnVanOrange\core\DB('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+ }
+ protected function tearDown(){
+  unset($this->db);
+ }
+ 
+ /**** fetch ****/
+ public function test_fetch_success() {
+  $query = new \Peyote\Select('report_types');
+  $result = $this->db->fetch($query);
+  $this->assertInternalType('array',$result);
+  $query = "SELECT * FROM report_types WHERE value = :value";
+  $val = array(
+   ':value' => 'NSFW'
+  );
+  $query = new \Peyote\Select('report_types');
+  $query->where('value', '=', 'NSFW');
+  $result = $this->db->fetch($query);
+  $this->assertInternalType('array',$result);
+ }
+ 
+}
