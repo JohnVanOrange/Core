aa19df5f08a03c12d6d6fca742c3c87566895b0c
Cameron Bulock <cameron@bulock.com>
2015-04-27 15:45:58 -0400

    removed unused function from DB class

--- src/API/DB.php
+++ src/API/DB.php
@@ -22,11 +22,6 @@ class DB extends PDO {
   return $s->fetchAll();
  }
 
- public function exec($sql) {
-  if (!$this->setup) $this->setup();
-  return parent::exec($sql);
- }
-
  private function setup() {
   $this->setup = TRUE;
   parent::__construct($this->dsn, $this->user, $this->pass, $this->dboptions);
